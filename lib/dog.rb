69e9f8ab6d6024d3ee9f342facec25601fa0d04f
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-20 01:03:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -84,8 +84,13 @@ class Dog
   
   def self.find_by_name(name)
     sql = <<-SQL
-    
+    SELECT *
+    FROM dogs
+    WHERE name = ?
     SQL
+    DB[:conn].execute(sql, name).collect do |row|
+      self.new_from_db(row)
+    end.first
   end
   
 end
